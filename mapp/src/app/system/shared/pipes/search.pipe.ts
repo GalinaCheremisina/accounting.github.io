bc12a9ac1f2cea919a773078f8b629482c07f74b
Vinojak <vinojak@yandex.ru>
2019-01-18 14:33:35 +0300

    version 1.2

--- mapp/src/app/system/shared/pipes/search.pipe.ts
+++ mapp/src/app/system/shared/pipes/search.pipe.ts
@@ -9,11 +9,10 @@ export class SearchPipe implements PipeTransform{
 
     transform(items: EventRecord[], value: string, field: string):any{
         if(items.length ===0 || !value) return items;
-
-        console.log(items);
-        console.log(field);
+        
         return items.filter((i)=>{
-            const t=Object.assign({},i);
+            const t = Object.assign({},i);
+            if(field === 'date'){ t[field] = t[field].substr(0,10); }
             if(!isNaN(t[field])) { t[field]+=''; }
             return t[field].toLowerCase().indexOf(value.toLowerCase())!==-1;
         });
